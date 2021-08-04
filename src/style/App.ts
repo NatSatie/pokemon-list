eff5f459a957a74048fd1bad602a4c9dcfe1ec5b
Natália Satie <naty.odashima@gmail.com>
2021-08-04 19:31:52 -0300

    style: create base style

---
+++ src/style/App.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  background-color: #282c34;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: calc(10px + 2vmin);
+  justify-content: center;
+  min-height: 100vh;
+`;

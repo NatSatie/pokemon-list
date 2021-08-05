5570e06d5fabdd892f358b511f46f33697dce8e7
Natália Satie <naty.odashima@gmail.com>
2021-08-05 10:48:23 -0300

    feat: add responsive search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,23 @@
 import Textfield from '@atlaskit/textfield';
 import { Container } from '../style/SearchBar';
+import usePokemon from '../hooks/usePokemon';
+import { FormEvent } from 'react';
 
 const SearchBar = () => {
+  const pocket = usePokemon();
+
+  const handleChange = (event: FormEvent<HTMLInputElement>) => {
+    pocket.setSearchInput(event.currentTarget.value);
+    
+  }
+
   return(
     <Container>
       <Textfield
         name="basic"
         aria-label="default text field"
         placeholder="Search pokémon"
+        onChange={handleChange}
       />
     </Container>
   )
